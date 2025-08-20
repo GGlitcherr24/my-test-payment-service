89700c228f2be40916bb8e23c5a09b19ff06f6fc
naa <nesterovantosha247@gmail.com>
2025-08-20 15:39:11 +0400

    add CORS Auth

---
+++ orders/web/api/auth.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+import jwt
+from cryptography.x509 import load_pem_x509_certificate
+
+public_key_text = (Path(__file__).parent / "../../../public_key.pem"
+                   ).read_text()
+public_key = load_pem_x509_certificate(public_key_text.encode()).public_key()
+
+
+def decode_and_validate_token(access_token):
+    """
+        Проверяет токен доступа. Если токен недействителен,
+        возвращает полезную нагрузку токена
+    """
+    return jwt.decode(
+        access_token,
+        key=public_key,
+        algorithms=["RS256"],
+        audience=["http://127.0.0.1:8000/orders"],
+    )
